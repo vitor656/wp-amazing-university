c3a225c83c7df79879b52090850dd689feebf346
Vitor <vitor.rabelo.656@gmail.com>
2017-11-29 11:36:01 -0300

    New Plugins/Custom fields relationship

--- app/public/wp-content/mu-plugins/university-post-types.php
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -1,5 +1,6 @@
 <?php
 function university_post_types(){
+    //Event Post Type
     register_post_type('event', array(
         'supports' => array(
             'title',
@@ -20,6 +21,46 @@ function university_post_types(){
         ),
         'menu_icon' => 'dashicons-calendar'
     ));
+
+    //Program Post Type
+    register_post_type('program', array(
+        'supports' => array(
+            'title',
+            'editor'
+        ),
+        'rewrite' => array(
+            'slug' => 'programs'
+        ),
+        'has_archive' => true,
+        'public' => true,
+        'labels' => array(
+            'name' => 'Programs',
+            'add_new_item' => 'Add New Program',
+            'edit_item' => 'Edit Program',
+            'all_items' => 'All Programs',
+            'singular_name' => 'Program'
+        ),
+        'menu_icon' => 'dashicons-awards'
+    ));
+
+    //Professor Post Type
+    register_post_type('professor', array(
+        'supports' => array(
+            'title',
+            'editor',
+            'thumbnail'
+        ),
+        'public' => true,
+        'labels' => array(
+            'name' => 'Professors',
+            'add_new_item' => 'Add New Professor',
+            'edit_item' => 'Edit Professor',
+            'all_items' => 'All Professors',
+            'singular_name' => 'Professor'
+        ),
+        'menu_icon' => 'dashicons-welcome-learn-more'
+    ));
+
 }
 
 add_action( 'init', 'university_post_types');
--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -14,9 +14,19 @@ function university_features(){
     // register_nav_menu( 'footerLocationOne', 'Footer Location One' );
     // register_nav_menu( 'footerLocationTwo', 'Footer Location Two' );
     add_theme_support( 'title-tag' );
+    add_theme_support('post-thumbnails');
+    add_image_size( 'professorLandscape', '400', '260', true );
+    add_image_size( 'professorPortrait', '480', '650', true );
+    add_image_size( 'pageBanner', '1500', '350', true );
 }
 
 function university_adjust_queries($query){
+    if(!is_admin() and is_post_type_archive( 'program' ) and $query->is_main_query()){
+        $query->set('orderby', 'title');
+        $query->set('order', 'ASC');
+        $query->set('posts_per_page', -1);
+    }
+
     if(!is_admin() and is_post_type_archive( 'event' ) and $query->is_main_query()){
         $today = date(Ymd);
         
